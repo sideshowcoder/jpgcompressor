e6e662fd225c46c86e37b25825056eb76826e47c
Philipp Fehre <philipp.fehre@googlemail.com>
2011-06-27 20:51:42 +0200

    Initial 0.1 Version, compression working

---
+++ Classes/ComSideshowcoderJpgcompressorModuleAssets.m
@@ -0,0 +1,15 @@
+/**
+ * This is a generated file. Do not edit or your changes will be lost
+ */
+#import "ComSideshowcoderJpgcompressorModuleAssets.h"
+
+extern NSData * dataWithHexString (NSString * hexString);
+
+@implementation ComSideshowcoderJpgcompressorModuleAssets
+
+- (NSData*) moduleAsset
+{
+	return nil;
+}
+
+@end
---
+++ example/app.js
@@ -0,0 +1,51 @@
+// This is a test harness for your module
+// You should do something interesting in this harness 
+// to test out the module and to provide instructions 
+// to users on how to use it by example.
+
+
+// open a single window
+var window = Ti.UI.createWindow({
+	backgroundColor:'white'
+});
+
+var f = Ti.Filesystem.getFile('test.jpg');
+var img = f.read.blob;
+var nImgView = Ti.UI.createImageView({
+  top: 0,
+  width: 200,
+  left: 20
+});
+
+var cImgView = Ti.UI.createImageView({
+  top: 200,
+  width: 100,
+  left: 20
+})
+
+window.add(nImgView);
+window.add(cImgView);
+window.open();
+
+// Init module
+var jpgcompressor = require('com.sideshowcoder.jpgcompressor');
+Ti.API.info('module is => ' + jpgcompressor);
+
+// Test
+
+// Set and read compress Factor
+Ti.API.info('Set compress Factor to 0.5...');
+jpgcompressor.setCompressFactor(0.1);
+Ti.API.info('Compress Factor: ' + jpgcompressor.compressFactor);
+
+// Compress an image
+var cImg = jpgcompressor.compress(img);
+nImgView.setImage(img);
+cImgView.setImage(cImg);
+
+
+
+
+
+
+
